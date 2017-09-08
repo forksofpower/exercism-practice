bff77aa73a432d8b4022b836df4a15cc6e45d553
Patrick Jones <forksofpower@gmail.com>
2017-09-08 16:00:36 -0500

    finished python rna_transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,16 @@
-def to_rna():
-    pass
+complement = {
+    'G': 'C',
+    'C': 'G',
+    'T': 'A',
+    'A': 'U'
+}
+
+def to_rna(dna):
+    output = ''
+
+    for c in dna:
+        if complement.has_key(c):
+            output = output + complement[c]
+        else:
+            return ''
+    return output
