73b819253a5fa824df08fe4cae84e5709da0467c
Patrick Jones <forksofpower@gmail.com>
2017-09-08 16:21:12 -0500

    finished python hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,7 @@
-def distance():
-    pass
+def distance(a, b):
+    if (len(a) != len(b)):
+        raise ValueError('sequences are not equal length')
+
+    # use sum to count how many times values are different
+    # this is summing bools (True = 1, False = 0)
+    return sum ( a[i] != b[i] for i in range(len(a)) )
